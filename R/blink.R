0cd8b6c8c0584ed1e1d9a2caf313ca9cee86f75f
tjebo <36016789+tjebo@users.noreply.github.com>
2020-05-20 22:00:06 +0100

    Update blink.R

--- R/blink.R
+++ R/blink.R
@@ -1,1 +1,11 @@
+#' Your data in a blink of an eye
+#' @description High level function, wrapping [myop], [eyes] and [insight]. 
+#'  Checks if data is in long format. If not, myop will be applied. 
+#'  eyes and insight will be applied on the result. 
+#' @name blink
+#' @param x data frame
+#' @details
+#' @examples
+#' @export
 
+blink <- function(x) {  }
