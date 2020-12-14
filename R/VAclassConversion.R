0112ec5546650cde5758c774cec0ddb14acd1361
tjebo <tjebn@web.de>
2020-12-14 15:52:31 +0000

    issue #6

---
+++ R/VAclassConversion.R
@@ -0,0 +1,31 @@
+#' VA class conversion
+#' @name VAclassConversion
+#' @description Simple convenience wrapper around [va] to get desired VA class
+#' @param x vector of visual acuities
+#' @param ... parameters passed to [va]
+#' @return vector with visual acuity of class to_(...)
+#'   See also [convertVA]: "VA classes"
+#' @family VA converter
+#' @export
+#'
+to_logmar <- function(x, ...){
+  va(x, to = "logmar", ...)
+}
+
+#' @rdname VAclassConversion
+#' @export
+to_etdrs <- function(x, ...){
+  va(x, to = "etdrs", ...)
+}
+
+#' @rdname VAclassConversion
+#' @export
+to_snellen <- function(x, ...){
+  va(x, to = "snellen", ...)
+}
+
+#' @rdname VAclassConversion
+#' @export
+to_quali <- function(x, ...){
+  va(x, to = "quali", ...)
+}
